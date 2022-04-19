a6d5a2854ca0589944c1c17bdd8df3915759d165
Mustafa <mustafasmnc@gmail.com>
2022-04-19 03:51:23 +0300

    make it better

--- index.js
+++ index.js
@@ -1,50 +1,85 @@
-const express = require('express');
-var http = require('http');
-const app = express();
-const port = process.env.PORT || 3000;
-var server = http.createServer(app);
-const mongoose = require('mongoose');
+// const express = require('express');
+// var http = require('http');
+// const app = express();
+// const port = process.env.PORT || 3000;
+// var server = http.createServer(app);
+// const mongoose = require('mongoose');
 
-const socketIO=require('socket.io');
+// const socketIO=require('socket.io');
 
-// var socket=require('socket.io');
-// var io=socket(server);
-var io = require('socket.io')(server);
+// // var socket=require('socket.io');
+// // var io=socket(server);
+// var io = require('socket.io')(server);
 
-//middleware
-app.use(express.json());
+// //middleware
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send("Node Server is running. Yay!!!")
+// })
+
+// //connect to DB 
+// const DB = 'mongodb+srv://smnc:smnc@cluster0.3ft7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+// mongoose.connect(DB).then(() => {
+//     console.log('Connection Succesful')
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+// const socketio = require('socket.io')(http)
+
+// socketio.on('connection', (client) => {
+//     console.log('a user connected')
+
+//     client.on('toServer', (data) => {
+//         console.log('Message received --> ', data)
+//         client.emit('fromServer', 'fromServer -> ' + data);
+//     })
+// })
+
+// // io.on('connection', function (socket) {
+// //     console.log(socket.id, 'joined');
+// //     socket.on('/test', function (msg) {
+// //         console.log(msg);
+// //     });
+// // });
+
+// app.listen(port, "0.0.0.0", () => {
+//     console.log(`Server running at ${port}`)
+// })
+
+const express = require('express')
+const app = express()
+const serverr = require('http').createServer()
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => {
+    console.log(`server is started on ${PORT}`)
+})
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!!")
 })
 
-//connect to DB 
-const DB = 'mongodb+srv://smnc:smnc@cluster0.3ft7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const io = require('socket.io')(server)
 
-mongoose.connect(DB).then(() => {
-    console.log('Connection Succesful')
-}).catch((e) => {
-    console.log(e)
-})
+const connectedUser = new Set();
 
-const socketio = require('socket.io')(http)
+io.on('connection', (socket) => {
+    console.log('Connected SocketIO Successfully', socket.id)
 
-socketio.on('connection', (client) => {
-    console.log('a user connected')
+    connectedUser.add(socket.id)
+    io.emit('connected-user', connectedUser.size)
 
-    client.on('toServer', (data) => {
-        console.log('Message received --> ', data)
-        client.emit('fromServer', 'fromServer -> ' + data);
+    socket.on('disconnect', () => {
+        console.log('Disconnected SocketIO', socket.id)
+        connectedUser.delete(socket.id)
+        io.emit('connected-user', connectedUser.size)
     })
-})
 
-// io.on('connection', function (socket) {
-//     console.log(socket.id, 'joined');
-//     socket.on('/test', function (msg) {
-//         console.log(msg);
-//     });
-// });
-
-app.listen(port, "0.0.0.0", () => {
-    console.log(`Server running at ${port}`)
+    socket.on('message', (data) => {
+        console.log(data)
+        //io.emit('message', data)
+        socket.emit('message-receive', data)
+    })
 })
