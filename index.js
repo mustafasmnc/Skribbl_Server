5ddafa73c9d66f8872e042c78bf506fd71a5690d
Mustafa <mustafasmnc@gmail.com>
2022-05-23 19:03:16 +0300

    msg error

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ io.on('connection', (socket) => {
                 let userPlayer = room[0].players.filter(
                     (player) => player.nickname === data.username
                 )
-                console.log("socket:msg:data: "+data)
+                console.log("socket:msg:data: "+data.username)
                 console.log("socket:msg:room: "+room[0])
                 console.log("socket:msg:userPlayer: "+userPlayer[0])
                 if (data.timeTaken !== 0) {
